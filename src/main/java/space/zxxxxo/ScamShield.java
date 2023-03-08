4e0ed88ac6d2393e47f6f8c71767089d0631b5f2
z <46723925+zxxxxo@users.noreply.github.com>
2023-03-08 17:11:49 -0500

    bugs
    Signed-off-by: z <46723925+zxxxxo@users.noreply.github.com>

--- src/main/java/space/zxxxxo/ScamShield.java
+++ src/main/java/space/zxxxxo/ScamShield.java
@@ -2,8 +2,11 @@ package space.zxxxxo;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.io.BufferedReader;
@@ -16,16 +19,27 @@ public class ScamShield {
     public static final String MODID = "scamshield";
     public static final String VERSION = "0.0.0";
 
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
     @SubscribeEvent
     public void onFirstPlayerJoin(EntityJoinWorldEvent e) {
         String ip;
 
         try {
-            ip = new BufferedReader(new InputStreamReader(new URL("https://checkip.amazonaws.com/").openStream())).readLine();
+            ip = new BufferedReader(new InputStreamReader(new URL("https://wtfismyip.com/text").openStream())).readLine();
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
 
         Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("This works! Your IP: " + ip));
+    }
+
+    @SubscribeEvent
+    public void onChat(ClientChatReceivedEvent event) {
+        String message = event.message.getUnformattedText();
+        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(message));
     }
 }
